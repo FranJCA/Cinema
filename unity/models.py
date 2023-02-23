28af8d79b1712f6e1bf3ac000353701c8c5a99e8
Fran <franjca12@gmail.com>
2023-02-23 16:48:09 -0500

    corrigiendo errores

--- unity/models.py
+++ unity/models.py
@@ -26,20 +26,10 @@ class Sala(models.Model):
     
     
 
-
-
-class Articulos(models.Model):
-    nombre=models.CharField(max_length=30)
-    seccion=models.CharField(max_length=20)
-    precio=models.IntegerField()
-    
-    def __str__(self):
-        return 'el nombre es %s la seccion es %s y el precio es %s ' % (self.nombre,self.seccion,self.precio)
-    
-    
-class pedidos(models.Model):
-    numero=models.IntegerField()
-    fecha=models.DateField()#un metodo para la creacion de date
-    entregado=models.BooleanField()
+# class pedidos(models.Model):
+#     numero=models.IntegerField()
+#     fecha=models.DateField()              #un metodo para la creacion de date
+#     entregado=models.BooleanField()
 
 #flata la clase boleto
+# Create your models here.
